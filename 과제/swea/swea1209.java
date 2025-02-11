81589c1612dd8b1048bfb3f0fa73d2b01ba03bcf
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-11 14:56:36 +0900

    Sum

--- 과제/swea/swea1209.java
+++ 과제/swea/swea1209.java
@@ -7,50 +7,51 @@ public class swea1209 {
         Scanner sc = new Scanner(System.in);
 
         // 배열 생성
-        int[][] arr = new int[101][101];
+        int[][] arr = new int[100][100];
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
                 arr[i][j] = sc.nextInt();
             }
         }
 
-        int mid = arr[50][50];
         int N = arr.length;
+
         // 행
         int rowMax = Integer.MIN_VALUE;
-        int row = 0; 
         for(int i=0;i<N;i++){
+            int row = 0; 
             for(int j=0;j<N;j++){
                 row += arr[i][j];
             }
             rowMax = Math.max(row, rowMax);
         }
+
         // 열
-        int colMax = Integer.MIN_VALUE;    // 열의 최대값
-        int col = 0;
+        int colMax = Integer.MIN_VALUE;    
         for(int i=0;i<N;i++){
+            int col = 0;
             for(int j=0;j<N;j++){
                 col += arr[j][i];
             }
             colMax = Math.max(col,colMax);
         }
 
-        int[] dxD = {-1,-1,1,1};
-        int[] dyD = {-1,1,1,-1};
-        
+
         // 오른쪽 대각선 
-        int rightDiagonal = 0; 
         // 왼쪽 대각선
+        int rightDiagonal = 0; 
         int leftDiagonal = 0;  
         for(int i=0;i<N;i++){
-            int nx = mid + dxD[i];
-            int ny = mid + dyD[i];
-            rightDiagonal += nx;
-            leftDiagonal += ny;
+            rightDiagonal += arr[i][i];
+            leftDiagonal += arr[i][N-i-1];
         }
 
-        
-        System.out.println(Math.max(Math.max(rowMax, colMax),Math.max(rightDiagonal, leftDiagonal)));
+        System.out.println(rowMax);
+        System.out.println(colMax);
+        System.out.println(rightDiagonal);
+        System.out.println(leftDiagonal);
+        int ans = Math.max(Math.max(rowMax, colMax), Math.max(rightDiagonal, leftDiagonal));
+        //System.out.println("#" + test_case+ " " + ans);
 
 
     }
