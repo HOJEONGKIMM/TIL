18f9cb05fbb58a5afa7d3e6a290a7cd49d2b74eb
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-14 07:33:14 +0900

    풍선

--- 과제/swea/swea9490.java
+++ 과제/swea/swea9490.java
@@ -31,20 +31,20 @@ public class swea9490 {
 			for(int j=0; j<M; j++) {
 				// 합
 				for(int round =0; round<arr[i][j]; round++) {
-					int sum = 0;
+					int sum = arr[i][j];
 					for(int a=0;a<4;a++) {
 						// 반복문 한 번 더 돌려야 되는 것 같음........ 
 						// 근데 조건 어케 써야될지 모르겠음
 						
-						int nx = arr[i][j] + dx[a];
-						int ny = arr[i][j] + dy[a];
+						int nx = i + round * dx[a];
+						int ny = j + round * dy[a];
 						
 						if(nx>=0 & nx<=N && ny>=0 && ny<=M) {
 							sum += nx + ny;
 							
 						}
 					}
-					ans = Math.max(maxSum, sum) - arr[i][j];
+					ans = Math.max(maxSum, sum) ;
 				}
 				
 			}
