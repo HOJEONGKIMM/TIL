07bdc234a3ed532083b4da1c3eb4f4a3a2a25556
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-14 16:51:06 +0900

    세로 출력

---
+++ 과제/swea/swea5356.java
@@ -0,0 +1,37 @@
+package 과제.swea;
+
+import java.util.Scanner;
+
+public class swea5356 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for(int t=1; t<=T; t++){
+            String[] arr= new String[5];
+
+            for(int i=0; i<arr.length; i++){
+                arr[i] = sc.nextLine();
+            }
+
+            int maxLen = Integer.MIN_VALUE;
+            int len = 0;
+            for(int i=0; i<arr.length; i++){
+                len = arr[i].length();
+                maxLen = Math.max(maxLen, len);
+            }
+            int size = arr.length;
+
+            char arr2 =' '; 
+            for(int i=0; i<maxLen; i++){
+                for(int j=0; j<size; j++){
+                    System.out.print(arr[j].charAt(i));
+                    arr2 = arr[j].charAt(i);
+                }
+            }
+            System.out.println("#" + t + " " + arr2);
+         }
+    }
+}
