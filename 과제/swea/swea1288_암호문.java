ab26b4bdd3741f288cbf5742f5fb2e9c2b9be7c4
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-19 15:38:46 +0900

    암호문1

---
+++ 과제/swea/swea1288_암호문.java
@@ -0,0 +1,32 @@
+package 과제.swea;
+
+public class swea1288_암호문 {
+    public static void main(String[] args) {
+       
+        
+
+
+
+
+    }
+
+    public class Node{
+        public int data;
+        public Node link;
+        
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    public void add(int index, int data){
+
+    }
+
+    public void isEmpty(){
+
+    }
+
+
+}
+
