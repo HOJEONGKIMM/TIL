160937683b7b28a9e5da39a009f27894f5d49f4e
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-20 17:29:19 +0900

    중위순회

---
+++ 과제/swea/swea1231.java
@@ -0,0 +1,17 @@
+package 과제.swea;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class swea1231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Map<Integer, Character> map = new HashMap<>();
+
+        
+    }
+}
