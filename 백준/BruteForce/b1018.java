ac184a81a2524e5465c4f8a6867b81aaa0445840
HOJEONGKIMM <khj15263245@gmail.com>
2025-02-27 14:57:19 +0900

    체스판 문제 수정 - 아직 오답

--- 백준/BruteForce/b1018.java
+++ 백준/BruteForce/b1018.java
@@ -12,12 +12,19 @@ public class b1018 {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        String[][] board = new String[N][M];
+        char[][] board = new char[N][M];
         boolean[][] visited = new boolean[N][M];
 
+        // for(int i=0; i<N; i++){
+        //     for(int j=0; j<M; j++){
+        //         board[i][j] = sc.nextLine();
+        //     }
+        // }
+
         for(int i=0; i<N; i++){
+            String line = sc.next();
             for(int j=0; j<M; j++){
-                board[i][j] = sc.nextLine();
+                board[i][j] =line.charAt(j);
             }
         }
 
@@ -25,26 +32,42 @@ public class b1018 {
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
 
-                for(int dir = 0; dir < 4; dir++){
-                    int nx = i + dx[dir];
-                    int ny = j + dy[dir];
+                int x = i;
+                int y = j;                
+                
+                while (true) { 
+                    
+                    int nextX = -1;
+                    int nextY = -1;
+                
+                    for(int dir = 0; dir < 4; dir++){
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
 
 
-                    if(board[i][j].equals("B")){
-                        if(nx>=0 && nx< N && !board[nx][ny].equals("W") && !visited[nx][ny]) {
-                            board[nx][ny] = "W";
-                            cnt++;
-                            visited[nx][ny] = true;
+                        if(board[x][y]=='B'){
+                            if(nx>=0 && nx< N && board[nx][ny]!='W' && !visited[nx][ny]) {
+                                board[nx][ny] = 'W';
+                                cnt++;
+                                visited[nx][ny] = true;
+                                nextX = nx; nextY = ny;
+                            }
+                        }else{
+                            if(nx>=0 && nx< N && board[nx][ny]!='B' && !visited[nx][ny]) {
+                                board[nx][ny] = 'B';
+                                cnt++;
+                                visited[nx][ny] = true;
+                                nextX = nx; nextY = ny;
+                            }
+
                         }
-                    }else{
-                        if(nx>=0 && nx< N && !board[nx][ny].equals("B") && !visited[nx][ny]) {
-                            board[nx][ny] = "B";
-                            cnt++;
-                            visited[nx][ny] = true;
+
+                        if(nextX==-1) {
+                            break;
                         }
+                        x = nextX; y = nextY;
 
                     }
-
                 }
 
             }
