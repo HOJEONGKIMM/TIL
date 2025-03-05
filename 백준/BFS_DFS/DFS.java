2021b7dee2dff9b9bce7d3c528af345f201cf055
HOJEONGKIMM <khj15263245@gmail.com>
2025-03-05 15:29:48 +0900

    Add DFS.java

---
+++ 백준/BFS_DFS/DFS.java
@@ -0,0 +1,53 @@
+package 백준.BFS_DFS;
+
+import java.util.*;
+
+class Edge{
+    int to;     // 연결된 정점
+    int weight; // 길이
+
+    Edge(int to, int weight){
+        this.to = to;
+        this.weight = weight;
+    }
+}
+
+
+public class DFS {
+    static int N;
+
+    static ArrayList<ArrayList<Edge>> adj;
+    static boolean[] visited;
+
+
+    static void dfs(ArrayList<ArrayList<Integer>> adj){
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<N-1; i++){
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        for(int i=0; i<N-1; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<N-1; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int C = sc.nextInt();
+
+            adj.get(A).add(new Edge(B, C));
+            adj.get(B).add(new Edge(A, C));
+        }
+
+
+    }
+}
