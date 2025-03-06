3fa24b8933aa52e722a2e8488b77d1d95d0713ea
HOJEONGKIMM <khj15263245@gmail.com>
2025-03-06 15:40:57 +0900

    너구리구구

--- 백준/BFS_DFS/DFS.java
+++ 백준/BFS_DFS/DFS.java
@@ -2,46 +2,25 @@ package 백준.BFS_DFS;
 
 import java.util.*;
 
-class Edge{
-    int to;     // 연결된 정점
-    int weight; // 길이
-
-    Edge(int to, int weight){
-        this.to = to;
-        this.weight = weight;
-    }
-}
-
-
 public class DFS {
+
     static int N;
 
-    static ArrayList<ArrayList<Edge>> adj;
+    static ArrayList<Edge>[] adj;
     static boolean[] visited;
-
-
-    static void dfs(ArrayList<ArrayList<Edge>> adj){
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<N-1; i++){
-
-
-            
-        }
-
-    }
-
+    static int[] distance;
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
 
-        for(int i=0; i<N-1; i++){
+        for (int i = 0; i < N - 1; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int i=0; i<N-1; i++){
+        for (int i = 0; i < N - 1; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
             int C = sc.nextInt();
@@ -50,6 +29,26 @@ public class DFS {
             adj.get(B).add(new Edge(A, C));
         }
 
+    }
 
+    static void dfs(ArrayList<ArrayList<Edge>> adj) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N - 1; i++) {
+
+        }
+
+    }
+}
+
+class Edge {
+
+    int to;     // 연결된 정점
+    int weight; // 길이
+
+    Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
 }
