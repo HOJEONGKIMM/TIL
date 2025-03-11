766354200e80b265d9267a288369dc670bb55dfd
HOJEONGKIMM <khj15263245@gmail.com>
2025-03-11 17:53:29 +0900

    N과 M(2)

---
+++ 백준/BackTracking/b15650.java
@@ -0,0 +1,21 @@
+package 백준.BackTracking;
+
+import java.util.Scanner;
+
+public class b15650 {
+
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        perm(0. 0);
+    }
+    
+    static void perm(int idx, int sidx) {
+        
+    }
+}
