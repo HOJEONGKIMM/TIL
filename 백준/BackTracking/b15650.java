a0d1ee9b4fed025023f8ca361805b7871c6e7637
HOJEONGKIMM <khj15263245@gmail.com>
2025-03-12 08:28:30 +0900

    15650 N과 M(2) - 조합문제

--- 백준/BackTracking/b15650.java
+++ 백준/BackTracking/b15650.java
@@ -4,18 +4,33 @@ import java.util.Scanner;
 
 public class b15650 {
 
+    static int N;
+    static int M;
     static int[] arr;
-    static boolean[] visited;
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        perm(0. 0);
+        arr = new int[M];
+
+        comb(0, 1);
     }
-    
-    static void perm(int idx, int sidx) {
-        
+
+    static void comb(int idx, int sidx) {
+        // 종료 조건
+        if (idx == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = sidx; i <= N; i++) {
+            arr[idx] = i;
+            comb(idx+1, i+1);
+        }
     }
 }
